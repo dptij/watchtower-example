2687a41d5dc3d74c4ee248af71f88a884bc3f66e
Dmitrii Pisarenko <d.pisarenko@jit.at>
2021-10-01 16:34:45 +0200

    Added a sample process. The web application seems to run. Now we have a Camunda application to containerize.

--- process-engine/src/main/java/at/jit/watchtowerdemo/WebappExampleProcessApplication.java
+++ process-engine/src/main/java/at/jit/watchtowerdemo/WebappExampleProcessApplication.java
@@ -1,9 +1,11 @@
 package at.jit.watchtowerdemo;
 
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableProcessApplication
 public class WebappExampleProcessApplication {
     public static void main(final String... args) {
         SpringApplication.run(WebappExampleProcessApplication.class, args);
