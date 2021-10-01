9a0472b932eb7bd1a3aa1b57adce8b9ee93e7e7b
Dmitrii Pisarenko <d.pisarenko@jit.at>
2021-10-01 16:26:46 +0200

    Added main class.

---
+++ process-engine/src/main/java/at/jit/watchtowerdemo/WebappExampleProcessApplication.java
@@ -0,0 +1,11 @@
+package at.jit.watchtowerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappExampleProcessApplication {
+    public static void main(final String... args) {
+        SpringApplication.run(WebappExampleProcessApplication.class, args);
+    }
+}
